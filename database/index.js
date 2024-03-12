2ff40b8b3055473ba286cf642a60393b186264a4
suraj <surajlokhande7744@gmail.com>
2024-03-12 08:57:41 +0530

    port

--- database/index.js
+++ database/index.js
@@ -1,4 +1,4 @@
-
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const port = 3000
@@ -11,6 +11,6 @@ app.get('/login', (req, res) => {
   res.send('login')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
